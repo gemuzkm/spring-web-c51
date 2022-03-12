ef900185230de39a70e0ca20c522971987c6331a
gemuz_work <gemuzkm@gmail.com>
2022-03-12 13:23:30 +0300

    add class TestInterceptor implements HandlerInterceptor

--- src/main/java/by/tms/entity/TestInterceptor.java
+++ src/main/java/by/tms/entity/TestInterceptor.java
@@ -1,4 +1,29 @@
 package by.tms.entity;
 
-public class TestInterceptor {
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getSession().getAttribute("user") != null) {
+            return true;
+        }
+        response.sendRedirect("/");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //		modelAndView.setViewName("error");
+        modelAndView.addObject("name", "Test");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }

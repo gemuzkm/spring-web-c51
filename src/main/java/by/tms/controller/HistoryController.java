fd9941c9d0690b9ffdbbd00ebab5c09d98ec4bc7
gemuz_work <gemuzkm@gmail.com>
2022-03-18 10:08:16 +0300

    class HistoryController init

---
+++ src/main/java/by/tms/controller/HistoryController.java
@@ -0,0 +1,9 @@
+package by.tms.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@RequestMapping("/history")
+public class HistoryController {
+}
